4a25f9e7687176af8cbfa09f53cace872ed9d1bd
androidZ200 <androidZ200daniilsamara@mail.ru>
2020-03-09 22:25:50 +0400

    Added fourier file

---
+++ MusicVizualizator/Fourier.cpp
@@ -0,0 +1,37 @@
+#include <complex>
+#include "MusicFile.h"
+#include <math.h>
+
+double dpi = acos(-1) * 2;
+
+double getY(MusicFile file, double x, bool isRightChannel) {
+	x *= file.header.sampleRate;
+	if (file.header.bitsPerSample == 8)
+	{
+		char* arr = file.samplesBuffer.arr;
+		if (file.header.numChannels == 1) return arr[(int)x];
+		else if (isRightChannel) return arr[(int)x * 2 + 1];
+		else return arr[(int)x * 2];
+	}
+	else 
+	{
+		short int* arr = file.samplesBuffer.arr;
+		if (file.header.numChannels == 1) return arr[(int)x];
+		else if (isRightChannel) return arr[(int)x * 2 + 1];
+		else return arr[(int)x * 2];
+	}
+	/*if (file.header.numChannels == 2)
+		if (isRightChannel)	x *= 2;
+		else x *= 2 + 1;
+	return file.samplesBuffer.arr[(int)x];*/
+}
+
+std::complex<double> integral(double a, double b, double freq, MusicFile file) {
+	std::complex<double> sum = 0;
+	double step = (b - a) / 4096;
+	for (double i = a; i <= b; i += step) {
+		double t = dpi * i * freq;
+		sum += getY(file, i, true) * std::complex<double>(cos(t), sin(t));
+	}
+	return sum;
+}
